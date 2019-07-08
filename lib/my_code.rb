b94182bce1ba9bfeeccc8edce43257dbb484357c
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-08 19:20:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,7 +44,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-    if array[i] == FALSE
+    if source_array[i] == FALSE
       return false
     end 
     i += 1 
