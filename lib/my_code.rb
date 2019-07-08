ce3e5976adf7cdeec0c6c47b0680d51006fde207
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-08 19:11:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,10 +53,3 @@ end
 
 
 
-describe "reduce_to_total returns a running total when given a starting point" do
-    it "reduces correctly" do
-      source_array = [1,2,3]
-      starting_point = 100
-      expect(reduce_to_total(source_array, starting_point)).to eq(106)
-    end
-  end
